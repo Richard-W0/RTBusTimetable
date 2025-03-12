1f3ad48270040a49f7f7ee1af5e7e98e82962c1d
Richard-W0 <rikuwong@hotmail.fi>
2025-03-12 11:14:16 +0200

    basic layout of the api calls

--- main.c
+++ main.c
@@ -6,11 +6,27 @@ int main(void){
   CURL *curl;
   CURLcode result;
   
+  //the HTTP request
   curl = curl_easy_init();
   if(curl == NULL){
-    fprintf(stderr, "HTTP request failed\n")
+    fprintf(stderr, "HTTP request failed\n");
     return -1;
   }
 
+  //set options for the HTTP request
+  curl_easy_setopt(curl, CURLOPT_URL, "https://mobility-api.mobility-database.fintraffic.fi/gtfs-realtime/v2/TripUpdates?apikey=YOUR_API_KEY");
+
+  //actually does the request
+  result = curl_easy_perform(curl);
+  if(result != CURLE_OK){
+    fprintf(sderr, "ERROR: %s\n", curl_easy_strerror(result));
+    return -1;
+  }
+
+
+
+
+  //closes the easy api session
+  curl_easy_cleanup(curl);
   return 0;
 }
